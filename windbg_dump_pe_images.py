d8a02a95a9e0556b3dc0e5c1a0b3729ae9c0a518
Jeong Wook Oh <oh.jeongwook@gmail.com>
2019-10-21 10:37:43 -0700

    Code refactored

--- windbg_dump_pe_images.py
+++ windbg_dump_pe_images.py
@@ -1,14 +1,18 @@
-from pykd import *
-import petool
+
 
 if __name__ == '__main__':
+    import os
     import sys
+    sys.path.append(os.path.join(os.path.dirname(os.__file__), 'site-packages'))
+
+    import petool
+    import pykd
 
     pattern = 'test'
-    filename= sys.argv[1]
+    filename= "output.dmp"
     tmp_filename = filename+".tmp"
 
-    lm_lines = dbgCommand("lm")
+    lm_lines = pykd.dbgCommand("lm")
     for line in lm_lines.splitlines():
         toks = line.split()
         
@@ -17,10 +21,10 @@ if __name__ == '__main__':
                 (start, end) = toks[0:2]
                 start = int(start, 16)
                 end = int(end, 16)
-                
-                dbgCommand(".writemem %s %x L?%x" % (tmp_filename, start, end-start))
-                dbgCommand("q")
 
-	pe_file = petool.PEFile(tmp_filename)
-	pe_file.FixPointerToRaw()
-	pe_file.Write(filename)
+                pykd.dbgCommand(".writemem %s %x L?%x" % (tmp_filename, start, end-start))
+                pykd.dbgCommand("q")
+
+    pe_file = petool.PEFile(tmp_filename)
+    pe_file.FixPointerToRaw()
+    pe_file.Write(filename)
